99f76f34abc144cdca9bef32d5b0ba8ca0baef69
Carlos Martel <carlos.martellamas@outlook.es>
2018-11-07 10:03:15 +0000

    Implemented some functionality of the data service.

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,2 +1,13 @@
 export class Book {
+  id: number;
+  title: string;
+  category: string;
+  numOfPages: number;
+  isFavourite: Boolean;
+  hasBeenRead: Boolean;
+  readDate: Date;
+
+  constructor(values: Object = {}){
+    Object.assign(this,values);
+  }
 }
--- src/app/booklist-data.service.ts
+++ src/app/booklist-data.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import {Book} from "./book";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooklistDataService {
-
+  nextId: number = 3;
+  books: Book[] = [
+                    new Book({id: 0, title: "Book 0", category: "Poetry", numOfPages: 100, isFavourite: false, hasBeenRead: false, readDate: null}),
+                    new Book({id: 1, title: "Book 1", category: "Drama", numOfPages: 50, isFavourite: false, hasBeenRead: false, readDate: null}),
+                    new Book({id: 2, title: "Book 2", category: "Science-Fiction", numOfPages: 200, isFavourite: false, hasBeenRead: false, readDate: null})
+                  ];
+  categories: string[] = ["Kids", "Comic", "Drama", "Poetry", "Science-Fiction", "Thriller", "Fantasy"];
   constructor() { }
+
+  getCategories():string[]{
+    return this.categories;
+  }
+  getBooks():Book[]{
+    return this.books;
+  }
+  addBookData(book: Book): BooklistDataService{
+    book.id = this.nextId++;
+    this.books.push(book);
+    return this;
+  }
+  // Simulate DELETE /books/:id
+  deleteBookById(id: number): BooklistDataService{
+
+    //me quedo con los libros que tengan una ID diferente a la que borro.
+    this.books = this.books.filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /books/:id
+  updateBookById(id: number, values: Object = {}): Book {
+    let book = this.getBookById(id);
+    if (!book) {
+      return null;
+    }
+    Object.assign(book, values);
+    return book;
+  }
+
+  //Simulate /books/:id
+  getBookById(id: number): Book{
+    let book = this.books.filter(book => book.id === id).pop();
+    return book;
+  }
 }
