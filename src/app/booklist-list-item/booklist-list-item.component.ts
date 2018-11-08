08bf1160edc5b881ca6de968f6b27447a1e5cfff
dwdm <carlos.martellamas@outlook.es>
2018-11-08 15:27:20 +0000

    Booklist-item implementation going on.

--- src/app/booklist-list-item/booklist-list-item.component.ts
+++ src/app/booklist-list-item/booklist-list-item.component.ts
@@ -16,7 +16,8 @@ export class BooklistListItemComponent implements OnInit {
 
   ngOnInit() {
   }
-
+//TODO crear una nueva instancia de un libro (newBook) que se volcará al servicio cuando querramos cambiar cosas del mismo.
+//TODO implementar el mismo select que hice para el header con las opciones de las categorías ya pickeadas (mirar lo del ngModel para hacerlo, creo que puedo tirar por ahí)
   toggleReadonly() {
     this.readonly = !this.readonly;
   }
